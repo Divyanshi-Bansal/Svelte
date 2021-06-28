6dd591d2762e667bb35a801c49d8fa68b3cc9096
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-29 01:41:05 +0530

    if-else in for-each

--- svelteApp/src/component/Conditionals.svelte
+++ svelteApp/src/component/Conditionals.svelte
@@ -11,13 +11,33 @@
         {name:"harry", age:9 , color:["black" , 'wheat']},
         {name:"anne", age:5 , color:["black" , 'wheat']}
     ]
+
+    let limit=0;
+
 </script>
 
 <main>
     <h1>Practice-6</h1>
     <h2>conditionls</h2>
     <h3>if-else</h3>
+    <input type="text" bind:value={limit}>
 
+    {#each array as a}
+        {#if a.age > limit}
+            <div>
+                <h4>{a.name}</h4>
+                <h5>{a.age}</h5>
+            </div>
+            {:else if a.age < limit}
+                <div>
+                    <h4>{a.name}</h4>
+                    <h5>{a.age}</h5>
+                </div>
+            {:else}
+                <p>Nothing to show...</p>
+                <h5>student of {a.age} is not present.</h5>
+        {/if}
+    {/each}
 </main>
 
 <atyle></atyle>
