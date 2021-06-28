8ded910f12ad819a866e8954cc8991de970fd1e8
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-28 21:53:32 +0530

    counter

---
+++ svelteApp/src/component/Counter.svelte
@@ -0,0 +1,10 @@
+<script></script>
+
+<main>
+    <h1>Practice-1</h1>
+    <h2>Counter</h2>
+    <button>CLick to increase</button>
+    <button>Click to decrease</button>
+</main>
+
+<style></style>
