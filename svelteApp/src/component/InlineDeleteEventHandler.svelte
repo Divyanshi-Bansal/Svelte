894323cc5c489321ac4d32c3df2a8a268c49fcff
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-29 01:11:46 +0530

    handle inline event

--- svelteApp/src/component/InlineDeleteEventHandler.svelte
+++ svelteApp/src/component/InlineDeleteEventHandler.svelte
@@ -1,10 +1,33 @@
 <script>
+    let details = [
+        {name:"Divyanshi" , id:1},
+        {name:"Priyanshi" , id:2},
+        {name:"Hariom" , id:3},
+        {name:"Deepanshu" , id:4},
+        {name:"Shalini" , id:5},
+        {name:"Anshika" , id:6},
+        {name:"Rahul" , id:7},
+        {name:"Anjali" , id:8},
+    ]
+
+    function DeleteById(id){
+        // we delete that particular record from original details so we used filter
+        details = details.filter((a) => id != a.id)
+    }
 
 </script>
 
  <main>
     <h1>Practice-5</h1>
     <h2>we will going to delete the particular record<br>by event handler. </h2>
+    <!-- for-each loop -->
+
+    {#each details as student}
+        <h4>student.name</h4>
+        <button on:click={() => DeleteById(student.id)}>Delete</button>
+        <!-- we can't pass DeleteById(id) directly in onclick just because it will be invoked directly when it prints, then delete suddenly, so  -->
+        <!-- to get rid from this automation, we use inline event handler through this we can pass arguments. -->
+    {/each}
  </main>
 
  <style>
