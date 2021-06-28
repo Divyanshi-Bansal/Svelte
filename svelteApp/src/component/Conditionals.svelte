5cea06fe5820f0590e756e461e24e40fa32849f4
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-29 01:30:13 +0530

    conditionals-if-else

---
+++ svelteApp/src/component/Conditionals.svelte
@@ -0,0 +1,16 @@
+<script>
+    let array = [
+        {name:"Divyanshi", age:20 , color:["black", 'red' , 'wheat']},
+        {name:"Priyanshi", age:17 , color:["black" , 'blue']},
+        {name:"Hariom", age:15 , color:["black" , 'wheat']}
+    ]
+</script>
+
+<main>
+    <h1>Practice-6</h1>
+    <h2>conditionls</h2>
+    <h3>if-else</h3>
+
+</main>
+
+<atyle></atyle>
