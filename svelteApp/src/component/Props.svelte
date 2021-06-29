2f10a316bde91cc407318fa68e725e672fa0cb5c
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-29 15:46:15 +0530

    export props value

--- svelteApp/src/component/Props.svelte
+++ svelteApp/src/component/Props.svelte
@@ -1,11 +1,26 @@
 <script>
-
+    export let message1 = "default message1";
+    export let message2 = "default message2";
 </script>
 
 <main>
     <h1>Practice-7</h1>
+    <h2>Props</h2>
+    <p>{message1}</p>
+    <h4>{message2}</h4>
 </main>
 
 <style>
-
+    h1{
+        color: green;
+    }
+    h2{
+        color: grey;
+    }
+    p{
+        color: indianred;
+    }
+    h4{
+        color: indigo;
+    }
 </style>
