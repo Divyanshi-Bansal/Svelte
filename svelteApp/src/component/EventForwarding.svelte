925729f775c33534ced5a5fe14756200ef8e00ca
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-06-29 22:31:17 +0530

    toggle function

--- svelteApp/src/component/EventForwarding.svelte
+++ svelteApp/src/component/EventForwarding.svelte
@@ -1,11 +1,19 @@
 <script>
-
+    let isDisplay = false;
 </script>
 
 <main>
     <h1>Practice-8</h1>
+    <h2>Event Forwarding</h2>
+    <h3>Description</h3>
+    {#if {isDisplay}}
+        <div class="alert">
+            <h3>Click anywhere to come out.</h3>
+        </div>
+    {/if}
 </main>
 
 <style>
 
 </style>
+
