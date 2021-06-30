60e4eee9529cfddfcfb621a0b6fa5ccdf665f37b
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-01 01:43:28 +0530

    select element on college name

--- svelteApp/src/component/Form.svelte
+++ svelteApp/src/component/Form.svelte
@@ -13,7 +13,7 @@ import { get_store_value } from "svelte/internal";
     // let B = false;
     // let C = false;
     // let D = false;
-    let courseSection = [];
+    let StudentDetail = [];
 
     const handleSubmit = () => {
         isDisplay = !isDisplay;
@@ -34,19 +34,27 @@ import { get_store_value } from "svelte/internal";
             <input type="checkbox" bind:checked={Bba}/> Bba <br>
             <input type="checkbox" bind:checked={Mba}/> Mba<br>
             <input type="checkbox" bind:checked={Mtech}/> Mtech <br> -->
-            <input type="checkbox" bind:group={courseSection} value="Btech"/> Btech <br>
-            <input type="checkbox" bind:group={courseSection} value="Bba"/> Bba <br>
-            <input type="checkbox" bind:group={courseSection} value="Mba"/> Mba<br>
-            <input type="checkbox" bind:group={courseSection} value="Mtech"/> Mtech <br>
+            <input type="checkbox" bind:group={StudentDetail} value="Btech"/> Btech <br>
+            <input type="checkbox" bind:group={StudentDetail} value="Bba"/> Bba <br>
+            <input type="checkbox" bind:group={StudentDetail} value="Mba"/> Mba<br>
+            <input type="checkbox" bind:group={StudentDetail} value="Mtech"/> Mtech <br>
             <label for="">Choose Section</label>
             <!-- <input type="checkbox" bind:checked={A}/> A <br>
             <input type="checkbox" bind:checked={B}/> B <br>
             <input type="checkbox" bind:checked={C}/> C <br>
             <input type="checkbox" bind:checked={D}/> D <br> -->
-            <input type="checkbox" bind:group={courseSection} value="A"/> A <br>
-            <input type="checkbox" bind:group={courseSection} value="B"/> B <br>
-            <input type="checkbox" bind:group={courseSection} value="C"/> C <br>
-            <input type="checkbox" bind:group={courseSection} value="D"/> D <br>
+            <input type="checkbox" bind:group={StudentDetail} value="A"/> A <br>
+            <input type="checkbox" bind:group={StudentDetail} value="B"/> B <br>
+            <input type="checkbox" bind:group={StudentDetail} value="C"/> C <br>
+            <input type="checkbox" bind:group={StudentDetail} value="D"/> D <br>
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label>Choose College</label>
+            <select bind:value={StudentDetail}>
+                <option value="GLA">GLA</option>
+                <option value="GL Bajaj">GL Bajaj</option>
+                <option value="IPU">IPU</option>
+                <option value="LPU">LPU</option>
+            </select>
             <button>Submit</button>
         </form>
     {/if}
@@ -58,7 +66,7 @@ import { get_store_value } from "svelte/internal";
             <h3>{Bba}</h3>
             <h3>{Mba}</h3>
             <h3>{Mtech}</h3> -->
-            {#each courseSection as cs}
+            {#each StudentDetail as cs}
                 <h3>cs</h3>
             {/each}
         </div>
