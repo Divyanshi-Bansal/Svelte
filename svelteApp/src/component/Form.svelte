66d10c9e95683550669df5fd6bbc53d116e93775
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-01 01:30:33 +0530

    show status of checkbox in boolean manner

--- svelteApp/src/component/Form.svelte
+++ svelteApp/src/component/Form.svelte
@@ -1,7 +1,19 @@
 <script>
+import { get_store_value } from "svelte/internal";
+
+
     let name;
     let isDisplay=true;
     let isShowData = false;
+    let Btech = false;
+    let Bba = false;
+    let Mtech = false;
+    let Mba = false;
+    let A = false;
+    let B = false;
+    let C = false;
+    let D = false;
+
     const handleSubmit = () => {
         isDisplay = !isDisplay;
         isShowData = !isDisplay;
@@ -17,15 +29,15 @@
             <label>Username:)</label>
             <input type="text" placeholder="enter name" bind:value={name}/>
             <label for="">Choose Course</label>
-            <input type="checkbox"/> Btech <br>
-            <input type="checkbox"/> Bba <br>
-            <input type="checkbox"/> Mba<br>
-            <input type="checkbox"/> Mtech <br>
+            <input type="checkbox" bind:checked={Btech}/> Btech <br>
+            <input type="checkbox" bind:checked={Bba}/> Bba <br>
+            <input type="checkbox" bind:checked={Mba}/> Mba<br>
+            <input type="checkbox" bind:checked={Mtech}/> Mtech <br>
             <label for="">Choose Section</label>
-            <input type="checkbox"/> A <br>
-            <input type="checkbox"/> B <br>
-            <input type="checkbox"/> C <br>
-            <input type="checkbox"/> D <br>
+            <input type="checkbox" bind:checked={A}/> A <br>
+            <input type="checkbox" bind:checked={B}/> B <br>
+            <input type="checkbox" bind:checked={C}/> C <br>
+            <input type="checkbox" bind:checked={D}/> D <br>
             <button>Submit</button>
         </form>
     {/if}
@@ -33,6 +45,10 @@
     <alert>Submitted Successfully!!</alert>
         <div class="data">
             <h2>{name}</h2>
+            <h3>{Btech}</h3>
+            <h3>{Bba}</h3>
+            <h3>{Mba}</h3>
+            <h3>{Mtech}</h3>
         </div>
     {/if}
 </main>
