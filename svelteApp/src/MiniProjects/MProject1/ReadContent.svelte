6b442451bdcfddb1fee3f5f302caa009beb0b91a
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-02 00:18:58 +0530

    styling the content page

--- svelteApp/src/MiniProjects/MProject1/ReadContent.svelte
+++ svelteApp/src/MiniProjects/MProject1/ReadContent.svelte
@@ -8,6 +8,11 @@
     const isReadMore = () => {
         ReadMore = true;
     }
+
+    const Login = (e) =>{
+        e.preventDefault();
+        navigate("/Form", { replace: true });
+    }
 </script>
 
 <!-- <Router>
@@ -25,20 +30,96 @@
             <h4 style="color: white;">For further Detais, <br> you can register yourself</h4>
             <h4 style="color: white;">It's Safe and Secure!!</h4>
         </Alert>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vel impedit autem? Eum, culpa similique! Excepturi mollitia iste hic ut doloremque. Voluptates, quae! Omnis odio cum quam iusto, reiciendis distinctio!</p>
         
-        <button on:click={isReadMore}>Read More</button>
+        <div class="second">
+            <div class="header">
+                <img src="" alt="logo"/>
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="">Customers</a>
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="">Contact</a>
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="" on:click={Login}>Log In</a>
+            </div>
+            <div class="sec-img"><img src="https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2.png" loading="lazy" height="500px" sizes="(max-width: 479px) 77vw, (max-width: 767px) 57vw, (max-width: 991px) 32vw, 100vw" srcset="https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2-p-500.png 500w, https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2-p-800.png 800w, https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2-p-1080.png 1080w, https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2-p-1600.png 1600w, https://uploads-ssl.webflow.com/5f57c471e9fc90819485fda5/600f9e8844a08a7009c7584c_Landing%20Page%20Stock%20Photos-2.png 1920w" alt="" class="image"></div>
+            <div class="content">
+                <h1>Grow Your business today by <br>launching your own loyalty program</h1>
+                <button on:click|preventDefault={isReadMore}>Want to know more...<br>Click For More</button>
+            </div>  
+        </div>
+        
+        
+        
     </div>
 </main>
 
 <style>
+    main{
+        background-color: #f5f1ea;
+    }
     .container{
         text-align: center;
         margin: 10% auto;
         justify-content: center;
         align-items: center;
+        background-color: #f5f1ea;
     }
     button{
         cursor: pointer;
+        background-color: #e5a68b;
+        color:white;
+        border-radius: 53px;        
+        padding: 20px;
+        font-size: 20px;
+    }
+    .header{
+        position: fixed;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: auto;
+        padding: 20px;
+        padding-bottom: 20px;
+        background-color: #e5a68b;
+    }
+
+    img{
+        /* border: 1px solid black; */
+        float: left;
+    }
+    .image{
+        float: right;
+        /* margin-right: 100px; */
+    }
+    a{
+        float: right;
+        padding: 10px;
+        margin-right: 10px;
+        color: white;
+    }
+    .second{
+        left: 0;
+        margin-top: 0px;
+        right: 0;
+        bottom: auto;
+        padding: 20px;
+        padding-bottom: 20px;
+    background-color: #f5f1ea;
+   
+    
+    }
+    .content{
+        align-items: center;
+        margin:150px;
+        margin-top: 180px;
+        /* border: 1px solid black; */
+        width:600px;
+        
+    }
+    h1 , h3{
+        font-family: 'Open Sans',sans-serif;
+    font-size: 40px;
+    line-height: 45px;
+
     }
 </style>
