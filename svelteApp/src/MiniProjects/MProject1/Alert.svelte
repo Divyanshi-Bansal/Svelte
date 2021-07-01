0d68fd20e3bb28c00d61281fca7be526aeec2d28
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-07-01 15:00:25 +0530

    akert message done

---
+++ svelteApp/src/MiniProjects/MProject1/Alert.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let isDisplay = true;
+    export let alertMessage = 'Kindly Register Yourself First :)';
+    const toggleDisplay = () =>{
+        isDisplay = !isDisplay;
+    }
+</script>
+
+<main>
+    {#if isDisplay}
+    <div class="alert">
+        <h3>{alertMessage}</h3>
+        <button on:click={toggleDisplay}>Okay!!</button>
+    </div>
+    {/if}
+</main>
+
+<style>
+    .alert{
+        border-color: white;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+    }
+    h3{
+        color: black;
+        margin: 10%;
+    }
+    button{
+        padding: 10px;
+        margin: 20px;
+        color:grey;
+        background-color: black;
+    }
+</style>
